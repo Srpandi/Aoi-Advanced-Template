ddc5dd11b0e84b42fad4c6f9cddb30d01e3af091
Srpandi <104927437+Srpandi@users.noreply.github.com>
2023-09-11 02:02:08 -0500

    Update index.js
    Replit options available and custom logs

--- index.js
+++ index.js
@@ -23,20 +23,43 @@ const bot = new AoiClient({
 const loader = new LoadCommands(bot);
 loader.load(bot.cmd, "./Handler/Commands/")
 
-bot.variables(require('./Handler/Main/Variables.js'), "main")
-bot.variables(require('./Handler/Main/Cases_Vars.js'), "cases") // Unique to the case system
+bot.variables(require('./Handler/Main/Variables.js'), "main") // For general variables
+bot.variables({
+    guild_cases: {}, // Object type variable, with which we save the cases with their information
+    cases: 0 // Number of cases
+}, "cases") // Unique to the case system
 
 bot.readyCommand({
+    $if: "old",
     code: `
 $log[
     ₊°︶︶︶︶︶︶︶︶︶︶︶︶︶ ‧₊˚
     Bot: $username[$clientID]
     Owner: $username[$clientOwnerIDs]
     Ping: $pingms
-    Invite me: "Invite your bot here"
-
+    $if[${config.logs}==true]
+Bot invite: https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot&permissions=1099511627775
+    $endif
         Handler By: SrPandi
     ━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━
+
+
 ]
 `
 })
+
+/* [{ REPLIT }] */
+
+if(config.replit.replit === true) {
+    const app = require('express');
+
+    app.get('/', (req, res) => {
+        res.send("Hi");
+    });
+
+    app.listen(config.replit.port, () => {
+        if(config.logs === true) {
+            console.log(`Connection was established correctly with port ${config.replit.port}`)
+        }
+    })
+}
