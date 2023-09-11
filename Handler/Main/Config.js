04027495d28421b6492d94f1cd5705dd1deb3ae3
Srpandi <104927437+Srpandi@users.noreply.github.com>
2023-09-11 01:58:12 -0500

    Update Config.js
    Now you can put your bot 24/7 with the new replit option

--- Handler/Main/Config.js
+++ Handler/Main/Config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    "token": "YOUR TOKEN HERE"
+    "token": "YOUR TOKEN HERE" // Your token bot
+    "replit": {
+        "replit": false, // Set "true" if you use replit and want to keep your bot 24/7
+        "port": 3000, // Port used 
+    }, // Requires express package
+    "logs": true // Enable if you want to see custom statuses, such as your bot invitation
 }
