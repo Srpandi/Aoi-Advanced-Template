923825fbed4943f76a8f279cb92be2786b1de22d
Srpandi <104927437+Srpandi@users.noreply.github.com>
2023-12-02 17:27:45 -0600

    Update View-Case.js

--- Handler/Commands/Moderation/View-Case.js
+++ Handler/Commands/Moderation/View-Case.js
@@ -9,24 +9,26 @@ module.exports = [{
     },
     code: `
 $author[$userTag[$get[mod]];$userAvatar[$get[mod]]]
-$description[**User:** $userTag[$getObjectProperty[case_$message[1].user]] ($getObjectProperty[case_$message[1].user])
-**Action:** $getObjectProperty[case_$message[1].action]
-**Reason:** $getObjectProperty[case_$message[1].reason]
-**Deleted?:** $getObjectProperty[case_$message[1].deleted]
+$description[**User:** $userTag[$getObjectProperty[case;case_$message[1].user]] ($getObjectProperty[case;case_$message[1].user])
+**Action:** $getObjectProperty[case;case_$message[1].action]
+**Reason:** $getObjectProperty[case;case_$message[1].reason]
+**Deleted?:** $getObjectProperty[case;case_$message[1].deleted]
 ]
-$color[$replaceText[$getObjectProperty[case_$message[1].action];warn;FFC005]]
+$color[$replaceText[$getObjectProperty[case;case_$message[1].action];warn;FFC005]]
 $footer[Case #$message[1]]
 
-$let[mod;$getObjectProperty[case_$message[1].mod]]
+$let[mod;$getObjectProperty[case;case_$message[1].mod]]
 
-$onlyIf[$getObjectProperty[case_$message[1]]!=undefined||$getObjectProperty[case_$message[1]]!=;:x: There is no case registered with that ID]
+$onlyIf[$getObjectProperty[case;case_$message[1]]!=undefined||$getObjectProperty[case;case_$message[1]]!=;:x: There is no case registered with that ID]
 
-$onlyIf[$message[1]>0&&$message[1]<=$djsEval[let obj = $nonEscape[$getObject[true]]
+$onlyIf[$message[1]>0;:x: There is no case registered with that ID]
+
+$onlyIf[$message[1]<=$djsEval[let obj = $nonEscape[$getObject[case;true]]
 Object.keys(obj).length;true];:x: There is no case registered with that ID]
 
 $onlyIf[$isNumber[$message[1]]==true;:x: Provide a valid case]
 
-$onlyIf[$getObject[false]!=;:x: There is no case to show]
+$onlyIf[$getObject[case;false]!=;:x: There is no case to show]
 
 $argsCheck[>;Missing arguments!
 \`\`\`
@@ -35,6 +37,6 @@ view-case <case ID>
 Enter a case to show
 \`\`\`]
 
-$createObject[$getGuildVar[guild_cases;$guildID;cases]]
+$createObject[case;$getGuildVar[guild_cases;$guildID;cases]]
 `
 }]
